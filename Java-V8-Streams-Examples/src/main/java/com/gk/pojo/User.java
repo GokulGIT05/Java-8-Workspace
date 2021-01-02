8ed42f63d8d55bab38662feb96f985175b4d137b
GokulGit05 <gkgopig02@gmail.com>
2021-01-03 02:54:06 +0530

    Refactored all the Java Projects to Maven Projects and set the compile & Source version to Java 8

---
+++ Java-V8-Streams-Examples/src/main/java/com/gk/pojo/User.java
@@ -0,0 +1,42 @@
+package com.gk.pojo;
+
+public class User {
+	
+	private int id;
+	private String name;
+	private int age;
+	private long salary;
+	public User(int id, String name, int age, long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	
+	
+}
